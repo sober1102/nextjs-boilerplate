10ed6a1bf452473910ea655d68096291b20c28e9
sober1102 <sobertoolhacks@yahoo.com>
2026-02-11 11:44:44 -0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        // This forces the router to use its dynamic matching engine
+        source: "/debug/:path*",
+        destination: "/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
